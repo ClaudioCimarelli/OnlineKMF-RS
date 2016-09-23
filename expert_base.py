7c9ac29f6f075b3b6356d3757790efe8dabaa7e1
ClaudioCimarelli <cima.claudio@gmail.com>
2016-09-23 16:48:55 +0200

    imf on clusters and rmse

--- expert_base.py
+++ expert_base.py
@@ -1,5 +1,6 @@
 from util import *
 from batch_MF import train
+from online_updates import user_update
 from cluster_build import build_clusters
 from user_based_predictions import user_based_pred
 
@@ -21,19 +22,31 @@ if __name__ == "__main__":
     M = len(batch_matrix[0])
     K = 40
     u_batch, v_batch = train(batch_matrix*mask, N, M, K, suffix_name='experts')
+    nz_ratings = non_zero_matrix(batch_matrix*mask)
+    bias = np.sum(batch_matrix*mask) / np.sum(nz_ratings)
 
     clusters, clusters_index = build_clusters(private_users_matrix)
 
-    rmse = np.zeros(11)
+    rmse_user_based = np.zeros(10)
+    rmse_imf = np.zeros(10)
 
-    for i, cluster in enumerate(clusters):
+    for index, cluster in enumerate(clusters):
+        ###train and test set per each cluster
         train_mask, test_mask = build_test_train_masks(cluster)
+        ###User based predictions
         pred = user_based_pred(cluster*train_mask)
-        rmse[i] = calc_rmse(cluster*test_mask, pred)
+        rmse_user_based[index] = calc_rmse(cluster * test_mask, pred)
+        ###IMF predictions
+        users_cluster = private_users_index[clusters_index[index]]
+        for user, i in enumerate(users_cluster):
+            profile_u = np.nonzero(batch_matrix[i, :] * train_mask[user, :])[0]
+            u_batch[i, :] = user_update(u_batch[i, :], v_batch[profile_u, :], bias, batch_matrix[i, profile_u])
+        f = np.dot(u_batch[users_cluster, :], v_batch.T) +bias
+        rmse_imf[index] = calc_rmse(cluster*test_mask, f)
 
     train_mask, test_mask = build_test_train_masks(batch_matrix)
     pred = user_based_pred(batch_matrix * train_mask)
-    rmse[10] = calc_rmse(batch_matrix * test_mask, pred)
+    rmse_usbas_tot = calc_rmse(batch_matrix * test_mask, pred)
     pass
 
 
